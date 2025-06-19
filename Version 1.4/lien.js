49d5a49e3be451d91b25c9e636ffb77bf04d223b
Roméo Leonard <romeo.leonard56@gmail.com>
2025-06-19 20:23:06 +0200

    Add files via upload

---
+++ Version 1.4/lien.js
@@ -0,0 +1,7 @@
+document.getElementById('lien-ENT').addEventListener('click', function () {
+    window.open('https://github.com/Alpha604/Fake-ENT');
+})
+
+document.getElementById('lien-Wiki').addEventListener('click', function () {
+    window.open('https://github.com/Alpha604/Wiki-Padia');
+});
