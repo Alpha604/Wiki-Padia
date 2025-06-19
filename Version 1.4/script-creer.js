6d408b67edef2f71f7463c3147ace1f7d2ff7473
Roméo Leonard <romeo.leonard56@gmail.com>
2025-06-19 20:22:29 +0200

    Delete Version 1.4 directory

--- Version 1.4/script-creer.js
+++
@@ -1,8 +0,0 @@
-// Creer bouton
-document.getElementById('retour').addEventListener('click', function () {
-    window.location.href = 'index.html';
-})
-
-document.getElementById('indisponible').addEventListener('click', function () {
-    window.location.href = 'index.html';
-})
