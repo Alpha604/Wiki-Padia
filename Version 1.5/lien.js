8679525684904dc1d68384b1b33f03b5eee546f6
Roméo Leonard <romeo.leonard56@gmail.com>
2025-06-19 20:22:38 +0200

    Delete Version 1.5 directory

--- Version 1.5/lien.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('lien-ENT').addEventListener('click', function () {
-    window.open('https://github.com/Alpha604/Fake-ENT');
-})
-
-document.getElementById('lien-Wiki').addEventListener('click', function () {
-    window.open('https://github.com/Alpha604/Wiki-Padia');
-});
