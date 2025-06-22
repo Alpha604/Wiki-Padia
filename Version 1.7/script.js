89e9475777fe9a90bb087e7ddbd072e6530a626f
Roméo Leonard <romeo.leonard56@gmail.com>
2025-06-22 21:13:45 +0200

    Add files via upload

---
+++ Version 1.7/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelectorAll('section').forEach(section => {
+                section.style.display = 'none';
+            });
+            document.querySelector(this.getAttribute('href')).style.display = 'block';
+        });
+    });
+})
+
+
+// Creer bouton
+document.getElementById('btn-creat').addEventListener('click', function () {
+    window.location.href = 'creer.html';
+})
+// Actu bouton
+
+document.getElementById('btn-actu').addEventListener('click', function () {
+    window.location.href = 'actu.html';
+    console.warn('Alert')
+})
+
+// Nav bouton
+document.getElementById('btn-nav').addEventListener('click', function () {
+    var nav = document.getElementById('nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none'
+    }
+    else {
+        nav.style.display = 'block'
+    }
+})
